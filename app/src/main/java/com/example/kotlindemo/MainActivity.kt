25e1bbc4b45b9a78c6681b7ddf1f792f4186eb79
32728 <medhini32728@gmail.com>
2021-06-01 19:05:36 +0530

    App Area of circle

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.kotlindemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var butn = findViewById<Button>(R.id.btn)
+        var txt = findViewById<TextView>(R.id.txt)
+        var input_result = findViewById<EditText>(R.id.input)
+
+        var flag=false;
+        butn.setOnClickListener(View.OnClickListener{
+            var radius : Double = input_result.text.toString().toDouble()
+            val pi=3.141
+            var area :Double= pi*radius*radius
+
+            txt.setText("Result : $area")
+        })
+
     }
 }
